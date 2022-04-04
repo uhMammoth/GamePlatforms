880151c8e7d60ab0e04a4f92bbc0cd1eef9c80a3
Capwonderbread <hectcast10@yahoo.com>
2022-04-03 22:00:39 -0700

    added droplist

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,47 @@
 // Get Game info
-function getRawgapi(game) {
-  var apiKey = ff8332b243a54f7db9e5249071a23ba5;
-  var apiUrl = "https://api.rawg.io/api/games/" + game + "?key=" + apiKey;
+var searchFormEl = document.querySelector("#searchBar");
+var gameInputEl = document.querySelector("#searchName");
 
-  fetch(apiUrl).then(function(response) {
-    return response.json();
-  })
-  .then(function (data) {
-    
-  })
+var gameSubmit = function(event) {
+  event.preventDefault();
+
+  var gameName = gameInputEl.value;
+  if (gameName) {
+    getRawgapi(gameName)
+    gameInputEl.value = "";
+  };
+  
 }
+
+function getRawgapi(gameName) {
+  var apiKey = "ff8332b243a54f7db9e5249071a23ba5";
+  var apiUrl = "https://api.rawg.io/api/games?key=" + apiKey +"&search="+ gameName.replace(/\s/g, "-") +"&search_exact=true";
+
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => displayGameData(data));
+}
+
+function dropDown(data){
+    $("#searchBar")
+  .append("<select name='game' id='listGames' class='w-1/4 p-3'></select>");
+  for(let i = 1; i < data.results.length; i++){
+  var gameName = data.results[i].name;
+  $("#listGames").append("<option value = '"+ gameName +"'>"+ gameName +"</option>");
+  }
+  
+}
+$("#listGames").change(console.log(this));
+
+function displayGameData(data) {
+  var img = data.background_image;
+  dropDown(data);
+  $("#gameImg").empty()
+  .append("<img src="+ img +">");
+  console.log(data);
+}
+
+
+
+
+searchFormEl.addEventListener("submit", gameSubmit)

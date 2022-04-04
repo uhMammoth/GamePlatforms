5689bdcf0bd8bfbb843407f4fb321916c2a5aa98
KyleP <kyl3p3tty@gmail.com>
2022-04-03 22:07:07 -0700

    test

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,37 @@
-//get list of newest games
-var apiCall = function(){
-    var apiKey = ff8332b243a54f7db9e5249071a23ba5;
-    var listGames = 'https://api.rawg.io/api/games?key=' + apiKey;
-    fetch(listGames)
-        .then(response => response.json())
-        .then(data => sideBarHandler(data));
+// get list of newest games
+// var apiCall = function(){
+//     var apiKey = ff8332b243a54f7db9e5249071a23ba5;
+//     var listGames = 'https://api.rawg.io/api/games?key=' + apiKey;
+//     fetch(listGames)
+//         .then(response => response.json())
+//         .then(data => sideBarHandler(data));
 
-}
+// }
 
-var sideBarHandler = function (data){
+// var sideBarHandler = function (data){
 
-    for (let index = 0; index < 5; index++) {
+//     for (let index = 0; index < 5; index++) {
         
-        createSideBarEl();
-        div title = data.name[i]
-        div.backgroundimg = data.thumbnail
-    }
+//         createSideBarEl();
+//         div title = data.name[i]
+//         div.backgroundimg = data.thumbnail
+//     }
     
-}
+// }
 
-var createSideBarEl = function (){
-    $('#sidebar').append('div');
-    div title
-}
+// var createSideBarEl = function (){
+//     $('#sidebar').append('div');
+//     div title
+// }
 
-var divClicked = function (){
-    searchInput = createSideBarEl.text
-}
+// var divClicked = function (){
+//     searchInput = createSideBarEl.text
+// }
 
-eventlistenerforsidebarEl
+// eventlistenerforsidebarEl
 // apiCall();
+var searchArray;
+funciton(data){
+
+    $('option').change(function);
+}

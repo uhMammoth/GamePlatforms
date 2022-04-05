f0dc259eb71a65f8843f37032609066030cbb4a2
Capwonderbread <hectcast10@yahoo.com>
2022-04-04 19:45:29 -0700

    edit html with id, added function to display data from api to html

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 var searchFormEl = document.querySelector("#searchBar");
 var gameInputEl = document.querySelector("#searchName");
 var searchedList;
-var apiUrl;
+
 var gameSubmit = function(event) {
     $('#listGames').remove();
     event.preventDefault();
@@ -27,6 +27,8 @@ function getRawgapi(gameName) {
 function dropDown(data){
     $("#searchBar")
   .append("<select name='game' id='listGames' class='w-1/4 p-3'></select>");
+  $("#listGames")
+  .append("<option>Select Game</option>");
   for(let i = 0; i < data.results.length; i++){
   var gameName = data.results[i].name;
   $("#listGames").append("<option value = '"+ i +"'>"+ gameName +"</option>");
@@ -36,28 +38,50 @@ function dropDown(data){
 
 
 function displayGameData(data) {
-    searchedList = data.results;
+  searchedList = data.results;
   // var img = data.background_image;
   dropDown(data);
-//   $("#gameImg").empty()
-//   .append("<img src="+ img +">");
+  
   console.log(data);
 }
 
-function gameHandler(data){
-    //get img title metascore platforms description
-    //add and create elements to main game display board
-    var img = data.background_image;
-    $("#gameImg").empty()
-  .append("<img src="+ img +">");
-}
-
 $(document).on('change', '#listGames', function(){
-    console.log(searchedList[$(this).val()]);
-    fetch(this.val)
-        .then(response => response.json())
-        .then(data => gameHandler(data));
+  var apiKey = "ff8332b243a54f7db9e5249071a23ba5";
+  var gameUrl = "https://api.rawg.io/api/games/"+ searchedList[$(this).val()].slug +"?key=" + apiKey;
+  
+  console.log(searchedList[$(this).val()].slug);
+  fetch(gameUrl)
+      .then(response => response.json())
+      .then(data => gameHandler(data));
 });
 
-searchFormEl.addEventListener("submit", gameSubmit);
+function gameHandler(data) {
+  //get img title metascore platforms description
+  //add and create elements to main game display board
+  var img = data.background_image;
+  console.log(data);
+  $("#gameImg").empty()
+  .append("<img src="+ img +">");
+  $(".container h1").empty()
+  .append(data.name);
+  $("#description").empty()
+  .append(data.description);
+  $("#metaScore").empty()
+  .append(data.metacritic);
+  $("#release").empty()
+  .append(data.released);
+   $("#listPlatform").empty()
+  for(let i = 0; i < data.parent_platforms.length; i++) {
+    var listPlatforms = data.parent_platforms[i].platform.name;
+   $("#listPlatform")
+    .append("<li>"+ listPlatforms +"</li>");
+  }
+  
+}
 
+
+
+
+
+
+searchFormEl.addEventListener("submit", gameSubmit)
